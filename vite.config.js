b7f6ef4d785a2ad1e9122266269b2b4c5d6206b9
黃士桓 <huan5678@gmail.com>
2022-04-10 22:53:19 +0800

    [upload] push github pages

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,14 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
-const root = resolve(__dirname, 'src');
-const outDir = resolve(__dirname, 'dist');
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  root,
   build: {
     minify: true,
-    outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        like: resolve(root, 'views/like.html'),
-        follow: resolve(root, 'views/follow.html'),
-        login: resolve(root, 'views/login.html'),
-        personal: resolve(root, 'views/personal.html'),
-        profile: resolve(root, 'views/profile.html'),
-        register: resolve(root, 'views/register.html'),
-        posts: resolve(root, 'views/posts.html'),
-      }
-    }
   },
   resolve: {
     alias: {
-      "@": root,
+      "@": resolve('src'),
     },
   },
   base: "./",
