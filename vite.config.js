5bc55b9ff1a0477a16dd0ad12bc60ff1a5cd6f27
黃士桓 <huan5678@gmail.com>
2022-04-10 22:35:21 +0800

    [fixed] vite path

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,31 @@
 import { defineConfig } from "vite";
-import path from "path";
+import { resolve } from "path";
+
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root,
   build: {
     minify: true,
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        like: resolve(root, 'views/like.html'),
+        follow: resolve(root, 'views/follow.html'),
+        login: resolve(root, 'views/login.html'),
+        personal: resolve(root, 'views/personal.html'),
+        profile: resolve(root, 'views/profile.html'),
+        register: resolve(root, 'views/register.html'),
+        posts: resolve(root, 'views/posts.html'),
+      }
+    }
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": root,
     },
   },
   base: "./",
